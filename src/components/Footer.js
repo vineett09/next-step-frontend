c8ae1bfe5d4d6a8b99b784002b12f8293c6a7942
Vineett09 <152068376+Vineett09@users.noreply.github.com>
2025-04-27 21:19:21 +0530

    improved ai suggestions and new footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaEnvelope } from "react-icons/fa";
 import "../styles/Footer.css";
 
 const Footer = () => {
@@ -9,6 +10,12 @@ const Footer = () => {
           &copy; {new Date().getFullYear()} My Personal Project. All rights
           reserved.
         </p>
+        <div className="contact-email">
+          <FaEnvelope className="email-icon" />
+          <a href="mailto:nextstep3738@gmail.com" className="email-link">
+            nextstep3738@gmail.com
+          </a>
+        </div>
       </div>
     </footer>
   );
